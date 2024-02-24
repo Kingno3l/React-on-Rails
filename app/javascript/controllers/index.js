058ac411e8f7b72e88fcd7653fd50e834b756bca
kingno3l <nuelluiz@gmail.com>
2024-02-24 18:51:24 +0100

    done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,31 +1,28 @@
-import { application } from "./application"
+import { Controller } from '@hotwired/stimulus';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import HelloController from './hello_controller';
+import application from './application';
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+/* eslint-disable import/extensions */
+import App from '../components/App'; // Assuming App is a JSX file, remove the extension
+import store from '../store/store'; // Remove the extension if it's unnecessary
+/* eslint-enable import/extensions */
 
-import { Controller } from "@hotwired/stimulus"
-import React from "react"
-import ReactDOM from 'react-dom'
-import { Provider } from "react-redux";
-
-<<<<<<< HEAD
-import App from "../components/App"
-import store from "../store/store";
-=======
-import App from "../App"
- import store from "../store";
->>>>>>> a610595a7eeb8c5ad24ec0bd69577414bb937a0d
+application.register('hello', HelloController);
 
 // Connects to data-controller="react-router"
 export default class extends Controller {
+  /* eslint-disable class-methods-use-this */
   connect() {
     const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
       <Provider store={store}>
         <App />
-      </Provider>
-    )
-
+      </Provider>,
+    );
   }
-} 
+/* eslint-enable class-methods-use-this */
+}
